85b8fba2fa93ba5c285dc162987a7ed238632a46
zeagle01 <zeagle01@163.com>
2020-09-01 15:35:28 +0800

    through put use ylog scale

--- plot_result.py
+++ plot_result.py
@@ -27,6 +27,7 @@ for i,case_name in enumerate(case_names):
 
 
     ax[1].set_xscale('log')
+    ax[1].set_yscale('log')
     ax[1].set_xlabel('size(byte)')
     ax[1].set_ylabel('bps(byte/s)')
     ax[1].plot(data[:,i*2+0],data[:,i*2+0]/data[:,i*2+1],'-o',label=case_name)
